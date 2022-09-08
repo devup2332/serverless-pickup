d542c99fcba6d4cf883f5dde0153fe9dca012f12
Diego Rojas <devup2332@gmail.com>
2022-09-08 01:22:49 -0500

    master: Added cors to all functions

--- src/functions/auth/loginUser.ts
+++ src/functions/auth/loginUser.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { loginUserController } from "../../controllers/services/users/loginUser";
-
+const ALLOWED_HOSTS = ["https://pickup-pi.vercel.app"];
 export const loginUser: APIGatewayProxyHandler = async (e) => {
+  const origin = e.headers.origin!;
+  let host = "";
+  if (ALLOWED_HOSTS.includes(origin)) {
+    host = origin;
+  }
   const { email, password } = JSON.parse(e.body!);
   const res = await loginUserController(email, password);
   return {
@@ -9,7 +14,7 @@ export const loginUser: APIGatewayProxyHandler = async (e) => {
     body: JSON.stringify(res),
     headers: {
       "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": origin,
       "Access-Control-Allow-Methods": "*",
       "Access-Control-Allow-Credentials": true,
       Accept: "*/*",
