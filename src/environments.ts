75309874e9fba03129963026f26632dc164957bd
Diego Rojas <devup2332@gmail.com>
2022-09-11 11:19:27 -0500

    master: Added esbuild and prisma

--- src/environments.ts
+++ src/environments.ts
@@ -1,10 +1,4 @@
 export const environments = {
-  DATABASE: {
-    DB_HOST: process.env.DB_HOST || "localhost",
-    DB_USERNAME: process.env.DB_USERNAME || "root",
-    DB_PASSWORD: process.env.DB_PASSWORD || "",
-    DB_DATABASE: process.env.DB_DATABASE || "",
-  },
   JSONWEBTOKENS: {
     JWT_SECRET: process.env.JWT_SECRET || "secret",
   },
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
 import server from "./graphql/apolloServer";
-import express from "express";
-import cors from "cors";
-export const handler = server.createHandler({
-  expressAppFromMiddleware: (middleware) => {
-    const app = express();
-    app.use(
-      cors({
-        origin: "*",
-      })
-    );
-    app.use(middleware);
-    return app;
-  },
-});
+export const handler = server.createHandler();
